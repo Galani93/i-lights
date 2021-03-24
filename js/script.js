7c8d693eda0e42620666a8fc6325b34e4adc38f7
aniga <ani_galstyan_93@mail.ru>
2021-03-24 21:08:33 +0300

    Main page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-var accItem = document.getElementsByClassName('accordionItem');
-var accHD = document.getElementsByClassName('accordionItemHeading');
-for (i = 0; i < accHD.length; i++) {
-    accHD[i].addEventListener('click', toggleItem, false);
-}
-function toggleItem() {
-    var itemClass = this.parentNode.className;
-    for (i = 0; i < accItem.length; i++) {
-        accItem[i].className = 'accordionItem close';
-    }
-    if (itemClass == 'accordionItem close') {
-        this.parentNode.className = 'accordionItem open';
-    }
-}
 
 $(function(){
     $('.header .menu-button').click(function() {
@@ -27,52 +13,3 @@ $(function(){
     });
 
 });
-const delay = 3000; //ms
-
-const slides = document.querySelector(".slides");
-const slidesCount = slides.childElementCount;
-const maxLeft = (slidesCount - 1) * 100 * -1;
-
-let current = 0;
-
-function changeSlide(next = true) {
-    if (next) {
-        current += current > maxLeft ? -100 : current * -1;
-    } else {
-        current = current < 0 ? current + 100 : maxLeft;
-    }
-
-    slides.style.left = current + "%";
-}
-
-let autoChange = setInterval(changeSlide, delay);
-const restart = function() {
-    clearInterval(autoChange);
-    autoChange = setInterval(changeSlide, delay);
-};
-
-// Controls
-document.querySelector(".next-slide").addEventListener("click", function() {
-    changeSlide();
-    restart();
-});
-
-document.querySelector(".prev-slide").addEventListener("click", function() {
-    changeSlide(false);
-    restart();
-});
-
-
-var Carousel = function (element, options) {
-
-    this.$indicators = this.$element.find('.carousel-indicators')
-}
-isCycling && this.pause()
-
-if (this.$indicators.length) {
-    this.$indicators.find('.active').removeClass('active')
-    var $nextIndicator = $(this.$indicators.children()[this.getItemIndex($next)])
-    $nextIndicator && $nextIndicator.addClass('active')
-}
-
-
