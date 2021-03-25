d6c789e9c300bbb45b40758cba6a845d8b4e6a27
aniga <ani_galstyan_93@mail.ru>
2021-03-25 21:28:13 +0300

    Card product page

--- js/script.js
+++ js/script.js
@@ -33,3 +33,17 @@ function setBubble(range, bubble) {
 
 
 }
+
+var numCount = document.getElementById('num_count');
+var plusBtn = document.getElementById('button_plus');
+var minusBtn = document.getElementById('button_minus');
+plusBtn.onclick = function() {
+    var qty = parseInt(numCount.value);
+    qty = qty + 1;
+    numCount.value = qty;
+}
+minusBtn.onclick = function() {
+    var qty = parseInt(numCount.value);
+    qty = qty - 1;
+    numCount.value = qty;
+}
