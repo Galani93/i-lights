fe21185ebd47db66d87264022e4b373b52dc87d3
aniga <ani_galstyan_93@mail.ru>
2021-03-30 20:41:54 +0300

    Company page adaptive

--- js/script.js
+++ js/script.js
@@ -31,31 +31,25 @@ function setBubble(range, bubble) {
     const newVal = Number(((val - min) * 100) / (max - min));
     bubble.innerHTML = val;
 
-
 }
-$(".tab_content").hide();
-$(".tab_content:first").show();
-/* в режиме вкладок */
-$("ul.tabs li").click(function () {
-    $(".tab_content").hide();
-    var activeTab = $(this).attr("rel");
-    $("#" + activeTab).fadeIn();
-    $("ul.tabs li").removeClass("active");
-    $(this).addClass("active");
-    $(".tab_accordion").removeClass("d_active");
-    $(".tab_accordion[rel^='" + activeTab + "']").addClass("d_active");
-});
-/* в режиме аккордеона */
-$(".tab_accordion").click(function () {
-    $(".tab_content").hide();
-    var d_activeTab = $(this).attr("rel");
-    $("#" + d_activeTab).fadeIn();
-    $(".tab_accordion").removeClass("d_active");
-    $(this).addClass("d_active");
-    $("ul.tabs li").removeClass("active");
-    $("ul.tabs li[rel^='" + d_activeTab + "']").addClass("active");
-});
-/* дополнительный класс tab_last,
-чтобы добавить границу к правой
-стороне последней вкладки. */
-$('ul.tabs li').last().addClass("tab_last");
+(function($){
+    $('#thumbcarousel').carousel(0);
+    var $thumbItems = $('#thumbcarousel .item');
+    $('#carousel').on('slide.bs.carousel', function (event) {
+        var $slide = $(event.relatedTarget);
+        var thumbIndex = $slide.data('thumb');
+        var curThumbIndex = $thumbItems.index($('#thumbcarousel .item.active').get(0));
+        if (curThumbIndex>thumbIndex) {
+            $('#thumbcarousel').one('slid.bs.carousel', function (event) {
+                $('#thumbcarousel').carousel(thumbIndex);
+            });
+            if (curThumbIndex === ($thumbItems.length-1)) {
+                $('#thumbcarousel').carousel('next');
+            } else {
+                $('#thumbcarousel').carousel(numThumbItems-1);
+            }
+        } else {
+            $('#thumbcarousel').carousel(thumbIndex);
+        }
+    });
+})(jQuery);
