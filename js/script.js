fee1c96b35719cc8105a7d5e388b0e9072c32331
aniga <ani_galstyan_93@mail.ru>
2021-04-11 20:04:40 +0400

    Catalog page filter

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ rangeSlider.addEventListener("input", showSliderValue, false);
 function showSliderValue() {
     rangeBullet.innerHTML = rangeSlider.value;
     var bulletPosition = (rangeSlider.value /rangeSlider.max);
-    rangeBullet.style.left = (bulletPosition * 432) + "px";
+    rangeBullet.style.left = (bulletPosition * 10) + "px";
 
 
 }
